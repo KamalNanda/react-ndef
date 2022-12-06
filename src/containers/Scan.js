59a40adefccce522e80370524b21e123cf117c00
Kamal Nanda <kamal.n@unboxrobotics.com>
2022-12-06 17:00:04 +0530

    commit

--- src/containers/Scan.js
+++ src/containers/Scan.js
@@ -28,7 +28,7 @@ const Scan = () => {
                     ndef.onreading = (event) => {
                         console.log("NDEF message read."); 
                         window.alert(JSON.stringify(event.serialNumber))
-                        window.alert(JSON.stringify(event.recordType))  
+                        window.alert(JSON.stringify(event.data))  
                         onReading(event);
                         setActions({
                             scan: 'scanned',
